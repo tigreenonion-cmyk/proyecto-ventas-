d0c9bb5856d58d9077279fb9a195ac1b9967cc53
tigreenonion-cmyk <tigreenonion@gmail.com>
2025-12-01 00:20:04 -0500

    Create SalesPage.js

---
+++ frontend/src/pages/SalesPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SaleForm from '../components/SaleForm';
+
+export default function SalesPage() {
+  return (
+    <div>
+      <h2>Registrar Venta</h2>
+      <SaleForm />
+    </div>
+  );
+}
