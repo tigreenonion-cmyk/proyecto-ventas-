82b10119c71337a58ecd4b97ae0951d905efc105
tigreenonion-cmyk <tigreenonion@gmail.com>
2025-12-01 00:15:55 -0500

    Create api.js

---
+++ rontend/src/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+
+const api = axios.create({
+  baseURL: API_URL + '/api',
+});
+
+// interceptor para agregar token
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;
