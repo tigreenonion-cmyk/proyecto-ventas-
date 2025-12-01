a0543b9a22e416391ce129ab6cdd455ae0dd1710
tigreenonion-cmyk <tigreenonion@gmail.com>
2025-12-01 00:10:50 -0500

    Update inventoryController.js

--- backend/controllers/inventoryController.js
+++ backend/controllers/inventoryController.js
@@ -4,3 +4,4 @@ const InventoryMovement = db.InventoryMovement;
 exports.listMovements = async (req, res) => {
   const movements = await InventoryMovement.findAll({ order: [['created_at','DESC']] });
   res.json(movements);
+};
